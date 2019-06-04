a1a28297ebf6c1f36c089a0b1b279b1f094b942a
duduNohr <duduuulessa@gmail.com>
2019-06-04 03:07:15 -0300

    Base do jogo feita.

--- src/forca.py
+++ src/forca.py
@@ -1,9 +1,32 @@
-def main():
-    print(__name__)
-
-def pizza():
-    print("fome")
+from readWords import randomWord
 
 if __name__ == "__main__":
-    main()
-    pizza()
+    erros = 0
+    palavra = ""
+    word = randomWord()
+    n = len(word)
+
+    for i in range(n):
+        palavra += "_"
+
+    print(palavra)
+
+    while(erros < 5):
+        x = input("Digite uma letra ou palavra caso queira chutar\n")
+
+        if len(x) > 1:
+            if x == word:
+                print("Voce Acertou! Parabens :D")
+                break
+            else:
+                print("Errou!")
+                erros += 1
+
+        if x in word:
+           for i, character in enumerate(word):
+               if x == character:
+                   palavra = palavra[:i] + x + palavra[i+1:]
+        else:
+            print("Letra nao encontrada")
+            erros += 1
+        print("Palavra: " , palavra, "\nerros: ", erros)
--- src/readWords.py
+++ src/readWords.py
@@ -1,8 +1,9 @@
 import random
-f = open("/home/18103819/gcs-icdc-travis/src/words/20words.txt","r")
+f = open("./words/20words.txt","r")
 
-palavras = []
-for line in f:
-    formatada = line.rstrip()
-    palavras.append(formatada)
-print(random.choice(palavras))
+def randomWord():
+    palavras = []
+    for line in f:
+        formatada = line.rstrip()
+        palavras.append(formatada)
+    return random.choice(palavras)
